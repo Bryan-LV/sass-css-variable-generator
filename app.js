0b95edb139175fcafe9eeb1e03a737936dbf2b46
Bryan Almaraz <balmaraz@apartments247.com>
2021-08-17 22:10:52 -0500

    more updates

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ function parseVariables(input) {
 function createObj(inputArr) {
     const cloneArr = [...inputArr];
     return cloneArr.reduce( (acc, cur, index) => {
+
         // split variable into the var name and its value
         let splitVariable = cur.split(':');
         let [varName, varValue] = splitVariable;
 
         // Remove prefix dollar signs if any
         let cleanVarName = varName.replace(/^\$+/, '');
+
         // then we create 2 objects 'scss' and 'css'
         let variableObj = {
             scss: `$${cleanVarName}: ${varValue.trim()} !default;`,
@@ -32,6 +34,7 @@ window.onload = function() {
     const copyBtn = document.querySelector('#copy-btn');
 
     form.addEventListener('submit', (e) => {
+
         e.preventDefault();
         // split up variables
         const vars = parseVariables(textInput.value);
@@ -45,19 +48,21 @@ window.onload = function() {
         // clear out before next run
         output.innerHTML = '';
         modData.forEach(obj => {
-            output.innerHTML += `<p class="scss-var output-var">${obj.scss}</p>`
+            output.innerHTML += `<p class="scss-var output-var">${obj.scss}</p>`;
         })
 
         modData.forEach(obj => {
-            output.innerHTML += `<p class="css-var output-var">${obj.css}</p>`
+            output.innerHTML += `<p class="css-var output-var">${obj.css}</p>`;
         })
     })
 
     copyBtn.addEventListener('click', function() {
+
         const variables = Array.from(document.querySelectorAll('.output-var'));
+
         const textToCopy = variables.reduce( function(accumulator, currentItem){
-            accumulator += `${currentItem.textContent}\n`
-            return accumulator
+            accumulator += `${currentItem.textContent}\n`;
+            return accumulator;
         }, ``);
 
         window.navigator.clipboard.writeText(textToCopy);
