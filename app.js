37d6b16485901d741a386396990394aeb991b48c
Bryan Almaraz <balmaraz@apartments247.com>
2021-08-17 22:04:08 -0500

    fixes

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ function parseVariables(input) {
     return input.split(regex);
 }
 
+// turn split variables into objects holding scss and css vars
 function createObj(inputArr) {
     const cloneArr = [...inputArr];
     return cloneArr.reduce( (acc, cur, index) => {
         // split variable into the var name and its value
         let splitVariable = cur.split(':');
         let [varName, varValue] = splitVariable;
+
+        // Remove prefix dollar signs if any
+        let cleanVarName = varName.replace(/^\$+/, '');
         // then we create 2 objects 'scss' and 'css'
         let variableObj = {
-            scss: `$${varName}: ${varValue.trim()} !default;`,
-            css: `--${varName}: #{${varName.trim()}};`
+            scss: `$${cleanVarName}: ${varValue.trim()} !default;`,
+            css: `--${cleanVarName}: #{$${cleanVarName.trim()}};`
         }
 
         acc[index] = variableObj;
@@ -23,16 +27,39 @@ function createObj(inputArr) {
 
 window.onload = function() {
     let textInput = document.querySelector('#input');
-    let button = document.querySelector('#button');
+    const form = document.querySelector('#form');
+    const output = document.querySelector('#output .output-text');
+    const copyBtn = document.querySelector('#copy-btn');
 
-    button.addEventListener('click', (e) => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
         // split up variables
         const vars = parseVariables(textInput.value);
-        // FIXME: trim last off array as it's always an empty string
+        
+        // FIXME: trim last item off array as it's always an empty string
         vars.pop();
-        // turn split variables into objects holding scss and css vars
-        const modData = createObj(vars);
-        console.log(modData);
 
+        // returns array of objects containing scss & css3 variables
+        const modData = createObj(vars);
+
+        // clear out before next run
+        output.innerHTML = '';
+        modData.forEach(obj => {
+            output.innerHTML += `<p class="scss-var output-var">${obj.scss}</p>`
+        })
+
+        modData.forEach(obj => {
+            output.innerHTML += `<p class="css-var output-var">${obj.css}</p>`
+        })
+    })
+
+    copyBtn.addEventListener('click', function() {
+        const variables = Array.from(document.querySelectorAll('.output-var'));
+        const textToCopy = variables.reduce( function(accumulator, currentItem){
+            accumulator += `${currentItem.textContent}\n`
+            return accumulator
+        }, ``);
+
+        window.navigator.clipboard.writeText(textToCopy);
     })
 }
